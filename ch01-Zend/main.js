cd62f3e2d53e1fbffce699d5fa8cc5ebfc6956d5
huynvn010193 <hpn010193@gmail.com>
2020-04-19 11:00:45 +0700

    Done clipBT01_CH02_3.DataBinDing

--- ch01-Zend/main.js
+++ ch01-Zend/main.js
@@ -1,6 +1,20 @@
 var vueInstance = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    title: 'Áo thun thể thao'
-  }
-})
+    title: "Áo thun thể thao vải đốm",
+    url: `https://www.lazada.vn/products/bo-3-ao-thun-tron-nam-form-rong-phong-cach-hang-quoc-vai-day-min-everest-trang-den-xam-i143044637-s147788288.html?spm=a2o4n.searchlistcategory.list.16.b5ea4153M6n7kF&search=1`,
+    target: "_blank",
+    price: 12000,
+    check: true,
+  },
+  methods: {
+    formatPrice() {
+      // this chính là đối tượng Vue hiện tại
+      var number = this.price;
+      return new Intl.NumberFormat("de-DE", {
+        style: "currency",
+        currency: "VND",
+      }).format(number);
+    },
+  },
+});
